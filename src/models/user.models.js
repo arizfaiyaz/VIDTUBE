e9b5d658ed04d484ff2e7680cce256d6e57a605b
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-02-22 13:50:47 +0530

    Add various new files and configurations to node modules

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema({
     username: {
@@ -45,7 +47,33 @@ const userSchema = new Schema({
     
 },
     { timestamps: true }
-)
+);
+
+userSchema.pre("save", async function (next){
+    if (this.modified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10);
+    
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccesstoken = function () {
+    //short lived access token
+    jwt.sign(
+        { 
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname    
+        },
+         process.env.ACCESS_TOKEN_SECRET,
+         { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+}
 
 
 export const User = mongoose.model("User", userSchema);
