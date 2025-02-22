fc26feb311d3f1533be6c661376f106eab73b574
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-02-22 12:31:59 +0530

    setup folders, schema, nodemon, few other things

---
+++ src/models/comment.models.js
@@ -0,0 +1,7 @@
+import mongoose, { Schema } from 'mongoose';
+
+const commentSchema = new Schema({
+    video: {
+        
+    }
+});
---
+++ src/models/like.models.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from 'mongoose';
+
+const likeSchema = new Schema({
+    video: {
+        type: Schema.Types.ObjectId,
+        red: 'Video',
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    },
+    tweet: {
+        type: Schema.Types.ObjectId,
+        ref: 'Tweet'
+    },
+    likedby: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    
+}, { timestamps: true });
